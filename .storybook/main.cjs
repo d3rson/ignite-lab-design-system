f3663d705e393cf5f9fa11cfc90b3b163c1ff417
Ederson F <d3rson.br@gmail.com>
2022-10-12 20:55:07 -0300

    sync package and package-lock

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,18 +3,23 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
+
   "framework": "@storybook/react",
+
   "core": {
     "builder": "@storybook/builder-vite"
   },
+
   "features": {
     "storyStoreV7": true
   },
+
   viteFinal:(config, { configType } ) => {
     if (configType === 'PRODUCTION'){
       config.base = '/ignite-lab-design-system/'
@@ -22,4 +27,5 @@ module.exports = {
 
     return config
   }
+  
 }
