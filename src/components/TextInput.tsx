0d645694553f1f43f808b69f60195f0fab82bfec
Ederson F <d3rson.br@gmail.com>
2022-10-12 23:10:56 -0300

    criação de uma página estática utilizando os componentes

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,7 +1,5 @@
-import { Slot } from "@radix-ui/react-slot";
-import { InputHTMLAttributes, ReactNode } from "react";
-
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+import { InputHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 export interface TextInputRootProps {
   children: ReactNode;
@@ -9,13 +7,13 @@ export interface TextInputRootProps {
 
 function TextInputRoot(props: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded  bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
       {props.children}
     </div>
   )
 }
 
-TextInputRoot.displayName = "TextInput.Root"
+TextInputRoot.displayName = 'TextInput.Root'
 
 export interface TextInputIconProps {
   children: ReactNode;
@@ -29,20 +27,23 @@ function TextInputIcon(props: TextInputIconProps) {
   )
 }
 
-TextInputIcon.displayName = "TextInput.Icon";
+TextInputIcon.displayName = 'TextInput.Icon'
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 function TextInputInput(props: TextInputInputProps) {
   return (
-      <input className="bg-transparent flex-1 outline-none text-gray-100 text-xs placeholder:text-gray-400" {...props} />
-  );
+    <input 
+      className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+      {...props}
+    />
+  )
 }
 
-TextInputInput.displayName = "TextInput.Input";
+TextInputInput.displayName = 'TextInput.Input'
 
 export const TextInput = {
   Root: TextInputRoot,
   Input: TextInputInput,
-  Icon: TextInputIcon
+  Icon: TextInputIcon,
 }
